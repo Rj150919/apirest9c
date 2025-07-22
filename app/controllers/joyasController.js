649954631801114bdebbeb8b1da999ba5a065bea
Rj150919 <181308874@utna.edu.mx>
2025-07-22 16:29:15 -0600

    Se realizo la funcion de actualizacion de Joyas

--- app/controllers/joyasController.js
+++ app/controllers/joyasController.js
@@ -14,6 +14,72 @@ function buscarTodo(req, res) {
 
 }
 
+function agregarjoya(req,res) {
+    new joyasModel(req.body)
+        .save()
+        .then(info => {
+            return res.status(200).send({mensaje: `La información se guardo con éxito`, info});
+        })
+        .catch(e => {
+            return res.status(404).send({ mensaje: `Error al agregar la joya ${e}`});
+        });
+}
+
+async function buscarJoya(req,res,next) {
+    if (!req.body)req.body={}
+    var consulta = {}
+    consulta[req.params.key] = req.params.value
+    await joyasModel.find(consulta)
+    .then(joyas => {
+        if (!joyas.length) return next();
+        req.body.joyas = joyas
+            
+        return next();
+    })
+    .catch (e => {
+        req.body.e = e;
+        return next()
+    })
+}
+
+function mostrarJoya(req,res) {
+    if (req.body.e) return res.status(404).send({mensaje:`error al buscar la información`})
+    if (!req.body.joyas) return res.status(204).send({mensaje:`no hay nada que mostrar`})
+    let joyas = req.body.joyas
+    return res.status(200).send({joyas})
+    
+}
+
+function eliminarJoya(req,res){
+    var joya = {}
+    joyas = req.body.joyas
+    joyasModel.deleteOne(joyas[0])
+    .then(info => {
+        return res.status(200).send({mensaje: "La información se elimino con éxito"});
+    })
+    .catch(e => {
+        return res.status(404).send({ mensaje: "Error al eliminar la joya",e});
+    });
+    }
+
+    function actualizarJoya(req, res) {
+        var joyas = {}
+        joyas = req.body.joyas
+        joyasModel.updateOne(joyas[0],req.body)
+        .then(info => {
+            return res.status(200).send({mensaje: "La información se actualizo con éxito"});
+        })
+        .catch(e => {
+            return res.status(404).send({ mensaje: "Error al actualizar la joya",e});
+        });
+    }
+
+
 module.exports = {
-    buscarTodo
+    buscarTodo,
+    agregarjoya,
+    buscarJoya,
+    mostrarJoya,
+    eliminarJoya,
+    actualizarJoya
 }
--- app/routes/joyasRoute.js
+++ app/routes/joyasRoute.js
@@ -2,5 +2,10 @@ const express = require('express');
 const router = express.Router();
 const joyasController = require('../controllers/joyasController');
 
-router.get('/', joyasController.buscarTodo);
+router.get('/', joyasController.buscarTodo)
+    .post('/', joyasController.agregarjoya)
+    .get('/:key/:value', joyasController.buscarJoya,joyasController.mostrarJoya)
+    .delete('/:key/:value',joyasController.buscarJoya,joyasController.eliminarJoya)
+    .put('/:key/:value',joyasController.buscarJoya,joyasController.actualizarJoya)
+
 module.exports = router;
